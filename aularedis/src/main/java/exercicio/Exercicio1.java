00f9db5e9b161e178096669944fafc617be845a0
MATHEUS KARPINSKI <matheus.karpinski@univille.br>
2026-05-13 19:37:08 -0300

    feat: exercicio1

---
+++ aularedis/src/main/java/exercicio/Exercicio1.java
@@ -0,0 +1,14 @@
+package exercicio;
+
+import redis.clients.jedis.RedisClient;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        RedisClient redis = RedisClient.create("redis://localhost:6379");
+        System.out.println(redis.ping());
+        //System.out.println(redis.echo("Olá mundo"));
+        System.out.println(redis.incr("programa:execucoes"));
+        redis.close();
+    }
+}
